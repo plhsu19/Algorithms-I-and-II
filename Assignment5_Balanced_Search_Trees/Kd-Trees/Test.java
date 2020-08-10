bb6a084febf0e509fab8b5134b34933457bc2c40
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-08-10 12:20:04 +0200

    create assignment6 Graphs: WordNet

--- Assignment5_Balanced_Search_Trees/Kd-Trees/Test.java
+++ Assignment5_Balanced_Search_Trees/Kd-Trees/Test.java
@@ -4,38 +4,15 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Test {
 
-    public static class Point {
-        private int x;
-        private int y;
-
-        public Point(int xc, int yc) {
-            x = xc;
-            y = yc;
-        }
-    }
-
-    public static class PointKey {
-        private Point p;
-        private boolean deleted;
-
-        public PointKey(Point pc) {
-            p = pc;
-            deleted = false;
-        }
-
-        public String toString() {
-            return "(x, y): " + p.x + " , " + p.y;
-        }
-    }
-
-
     public static void main(String[] args) {
-        Point pointTest = new Point(1, 2);
-        PointKey pointKeyTest = new PointKey(pointTest);
+        boolean a = true;
+        boolean b = false;
 
-        System.out.println(pointKeyTest);
+        StdOut.println(a && b);
 
 
     }
